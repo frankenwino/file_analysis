a430edcf2f561a7950acd0301a532ba0bbf55a8f
Andrew Browne <andy.maildrop@gmail.com>
2022-03-02 11:29:05 +0100

    Flake8 changes

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import json
-from pprint import pprint
 import configparser
 
 
@@ -10,13 +9,15 @@ def get_config(config_file_path):
 
     return config
 
+
 def now():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 def create_json_file(json_file_path, json_data):
     with open(json_file_path, "w") as f:
-         json.dump(json_data, f, indent=4)
+        json.dump(json_data, f, indent=4)
+
 
 def json_file_to_dict(json_file_path):
     with open(json_file_path, "r") as f:
