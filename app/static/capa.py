733e5df43e64d73dc351edc1c5c9d934510db06c
Andrew Browne <andy.maildrop@gmail.com>
2022-03-07 16:16:15 +0100

    New location

---
+++ app/static/capa.py
@@ -0,0 +1,54 @@
+import subprocess
+from pprint import pprint
+import json
+import utils
+import os
+import converter
+
+def capa(sample_file_path):
+    """
+    https://github.com/mandiant/capa/blob/master/doc/installation.md
+    https://github.com/mandiant/capa-rules
+    """
+    print(f"{utils.now()} - capa scan in progress...")
+    capa_rules_dir = os.path.join(file_dir, "capa-rules")
+    capa_sigs_dir = os.path.join(file_dir, "capa-sigs")
+
+    # subprocess.call([
+    #     "capa",
+    #     "-jq",
+    #     "-r", capa_rules_dir,
+    #     "-s", capa_sigs_dir,
+    #     sample_file_path
+    #     ])
+
+    output = subprocess.check_output([
+        "capa",
+        "-jq",
+        "-r", capa_rules_dir,
+        "-s", capa_sigs_dir,
+        sample_file_path
+    ])
+    output_dict = json.loads(output.decode("utf-8"))
+
+    serialised_output_dict = converter.serialize(output_dict)
+
+    # pprint(output_dict, indent=4)
+
+    report_file_path = os.path.join(report_dir, "capa.json")
+    utils.create_json_file(report_file_path, output_dict)
+
+    print(f"{utils.now()} - capa scan complete")
+
+    return serialised_output_dict
+
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+file_dir_split = os.path.split(file_dir)
+report_dir = os.path.join(file_dir_split[0], "reports")
+if not os.path.isdir(report_dir):
+    os.makedirs(report_dir)
+
+if __name__ == "__main__":
+    sample_file_path = os.path.join(file_dir_split[0], "sample", "Setup.exe")
+    capa(sample_file_path)
---
+++ app/static/manalyze.py
@@ -0,0 +1,55 @@
+import utils
+import subprocess
+from pprint import pprint
+import json
+import os
+import converter
+
+"""
+https://docs.manalyzer.org/en/latest/index.html
+https://github.com/JusticeRage/Manalyze
+
+Install manalyze
+
+$ sudo apt install libboost-regex-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libssl-dev build-essential cmake git
+$ git clone https://github.com/JusticeRage/Manalyze.git && cd Manalyze
+$ cmake .
+$ make install -j5
+$ manalyze --version
+# $ cd bin && ./manalyze --version
+"""
+
+
+def manalyze(file_path):
+    print(f"{utils.now()} - manalyze scan in progress...")
+    # this_file = utils.get_file_method(__file__, sys._getframe(  ).f_code.co_name)
+    # print(f"{utils.now()} - {this_file}")
+
+    # subprocess.call(["manalyze", "-dall", file_path, "-o", "json"])
+    # output = subprocess.check_output(["manalyze", "-dall", file_path, "-o", "json"])
+    output = subprocess.check_output(["manalyze", "-d", "all", "-p", "all", "--pe", file_path, "-o", "json"])
+    output_dict = json.loads(output.decode("utf-8"))
+
+    print(f"{utils.now()} - manalyze scan complete")
+
+    serialised_output_dict = converter.serialize(output_dict[file_path])
+
+    return serialised_output_dict  # [file_path]
+
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == "__main__":
+    file_dir_split = os.path.split(file_dir)
+    sample_dir = os.path.join(file_dir_split[0], "sample")
+    sample_file_path = os.path.join(sample_dir, "Setup.exe")
+
+    manalyze_dict = manalyze(sample_file_path)
+
+    # pprint(manalyze_dict, indent=4)
+
+    report_dir = os.path.join(file_dir_split[0], "reports")
+    if not os.path.isdir(report_dir):
+        os.makedirs(report_dir)
+    report_file = os.path.join(report_dir, "manalyze.json")
+    utils.create_json_file(report_file, manalyze_dict)
