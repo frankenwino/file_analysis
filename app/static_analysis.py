8cf4b9cbf9797fd4c7a32004b952cc92fec3c5e3
Andrew Browne <andy.maildrop@gmail.com>
2022-03-07 17:51:47 +0100

    Added flarestrings capability

--- app/static_analysis.py
+++ app/static_analysis.py
@@ -3,22 +3,32 @@ import static.capa as capa
 import static.manalyze as manalyze
 import pymongo
 import static.file_info as file_info
+import static.flare_strings as flare_strings
 
 sample_coll = database.static_analysis_db.test
 sample_coll.create_index([("md5", pymongo.ASCENDING)], unique=True)
 
 if __name__ == "__main__":
     sample_path = "/home/andy/Desktop/Setup.exe"
-    capa_dict = capa.capa(sample_path)
-    manalyze_dict = manalyze.manalyze(sample_path)
     file_info_object = file_info.FileInfo(sample_path)
     file_info_dict = file_info_object.all_file_info_not_none()
+    string_ranks = flare_strings.flare(sample_path)
+
+    capa_dict = capa.capa(sample_path)
+    manalyze_dict = manalyze.manalyze(sample_path)
 
     doc = {
         "md5": file_info_dict["md5"],
         "file_info": file_info_dict,
         "capa": capa_dict,
-        "manalyze": manalyze_dict
-        }
+        "manalyze": manalyze_dict,
+        "string_rank": string_ranks
+    }
+
+    # doc = {
+    #     "md5": file_info_dict["md5"],
+    #     "file_info": file_info_dict,
+    #     "string_rank": string_ranks
+    #     }
 
     sample_coll.insert_one(doc)
