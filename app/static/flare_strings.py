cb91d3f8189f9223ef1217053c3c3f8293ce74a6
Andrew Browne <andy.maildrop@gmail.com>
2022-03-08 10:49:13 +0100

    Returns a dictionary instead of a list.

--- app/static/flare_strings.py
+++ app/static/flare_strings.py
@@ -1,20 +1,20 @@
 import subprocess
-# import utils
+from . import utils_cheat
 
 def flare(file_path):
 
-    # print(f"{utils.now()} - flarestrings starting")
-    print("flarestrings starting")
+    print(f"{utils_cheat.now()} - flarestrings starting")
+    # print("flarestrings starting")
 
     flarestrings = subprocess.Popen(("flarestrings", "-n", "4", file_path), stdout=subprocess.PIPE)
     output = subprocess.check_output(("rank_strings", "-s"), stdin=flarestrings.stdout).decode("utf-8")
     flarestrings.wait()
 
-    # print(f"{utils.now()} - flarestrings complete")
-    print("flarestrings complete")
+    print(f"{utils_cheat.now()} - flarestrings complete")
+    # print("flarestrings complete")
 
     split_output = output.split("\n")
-    output_list = []
+    output_dict = {}
     for line in split_output:
         split_line = line.split(",")
         rank = split_line[0].strip()
@@ -22,19 +22,19 @@ def flare(file_path):
 
         if len(rank) > 0 and len(the_string) > 0:
             rank_dict = {rank: the_string}
-            output_list.append(rank_dict)
+            output_dict[rank] = the_string
 
 
 
-    return output, output_list
+    return output_dict
 
 
 if __name__ == '__main__':
     from pprint import pprint
     file_path = "/home/andy/Desktop/Setup.exe"
-    flare_output, flare_output_list = flare(file_path)
+    flare_output_dict = flare(file_path)
     # pprint(flare_output, indent=4)
     # with open("flare.txt", "w") as f:
     #     f.write(flare_output)
-    pprint(flare_output_list, indent=4)
-    # utils.create_json_file("flare.json", flare_output_list)
+    pprint(flare_output_dict, indent=4)
+    utils_cheat.create_json_file("flare.json", flare_output_dict)
