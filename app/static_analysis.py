2e47332be99741515ba1c8e5b63db1e5fe32bf81
Andrew Browne <andy.maildrop@gmail.com>
2022-03-08 10:49:24 +0100

    Comment/uncomment while testing

--- app/static_analysis.py
+++ app/static_analysis.py
@@ -14,24 +14,25 @@ if __name__ == "__main__":
     file_info_object = file_info.FileInfo(sample_path)
     file_info_dict = file_info_object.all_file_info_not_none()
 
-    # string_ranks = flare_strings.flare(sample_path)
+    flare_strings_output_dict = flare_strings.flare(sample_path)
+
     # capa_dict = capa.capa(sample_path)
     # manalyze_dict = manalyze.manalyze(sample_path)
-    
-    peframe_dict = peframe.peframe(sample_path)
-
+    # peframe_dict = peframe.peframe(sample_path)
+    #
     # doc = {
     #     "md5": file_info_dict["md5"],
     #     "file_info": file_info_dict,
     #     "capa": capa_dict,
     #     "manalyze": manalyze_dict,
+    #     "peframe": peframe_dict,
     #     "string_rank": string_ranks
     # }
 
     doc = {
         "md5": file_info_dict["md5"],
         "file_info": file_info_dict,
-        "peframe": peframe_dict
+        "string_ranks": flare_strings_output_dict
         }
 
     sample_coll.insert_one(doc)
