08f8593fcf2b653ba0661de16a6168dd4755320d
Andrew Browne <andy.maildrop@gmail.com>
2022-03-08 16:35:20 +0100

    Added screen output

--- app/static/peframe.py
+++ app/static/peframe.py
@@ -7,14 +7,19 @@ sudo apt install peframe
 import subprocess
 from pprint import pprint
 import json
+from . import utils_cheat
+
 
 def peframe(file_path):
+    print(f"{utils_cheat.now()} - peframe scan in progress...")
     output = subprocess.check_output(["peframe", "--json", file_path])
     output_dict = json.loads(output.decode("utf-8"))
+    print(f"{utils_cheat.now()} - peframe complete")
 
-    pprint(output_dict, indent=4)
+    # pprint(output_dict, indent=4)
 
     return output_dict
+
 
 if __name__ == '__main__':
     file_path = "/home/andy/Desktop/Setup.exe"
